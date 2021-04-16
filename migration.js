7789b4a3a078ff1bca1c15ae2a2cb30c40919a88
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2021-04-16 18:33:18 +0300

    fix: new and old configs in same file

--- migration.js
+++ migration.js
@@ -12,38 +12,35 @@ fs.readFile(path.resolve(jsonPath, 'vedirect-signalk.json'), 'UTF-8', (err, data
 
   if (typeof obj.configuration.vedirect == 'undefined' ) {
     if (obj.configuration.device) {
-      obj.configuration = {
-        'vedirect':[{
-          'device':'Serial',
-          'connection': obj.configuration.device,
-          'port': '',
-          'ignoreChecksum': obj.configuration.ignoreChecksum,
-          'mainBatt': obj.configuration.mainBatt,
-          'auxBatt': obj.configuration.auxBatt,
-          'solar': obj.configuration.solar
-        }]}
+      obj.configuration.vedirect = [{
+        'device':'Serial',
+        'connection': obj.configuration.device,
+        'port': '',
+        'ignoreChecksum': obj.configuration.ignoreChecksum,
+        'mainBatt': obj.configuration.mainBatt,
+        'auxBatt': obj.configuration.auxBatt,
+        'solar': obj.configuration.solar
+      }]
     } else if (obj.configuration.udpPort) {
-      obj.configuration = {
-        'vedirect':[{
-          'device':'UDP',
-          'connection': 'localhost',
-          'port': obj.configuration.udpPort,
-          'ignoreChecksum': obj.configuration.ignoreChecksum,
-          'mainBatt': obj.configuration.mainBatt,
-          'auxBatt': obj.configuration.auxBatt,
-          'solar': obj.configuration.solar
-        }]}
+      obj.configuration.vedirect = [{
+        'device':'UDP',
+        'connection': 'localhost',
+        'port': obj.configuration.udpPort,
+        'ignoreChecksum': obj.configuration.ignoreChecksum,
+        'mainBatt': obj.configuration.mainBatt,
+        'auxBatt': obj.configuration.auxBatt,
+        'solar': obj.configuration.solar
+      }]
     } else if (obj.configuration.host) {
-      obj.configuration = {
-        'vedirect':[{
-          'device':'TCP',
-          'connection': obj.configuration.host,
-          'port': obj.configuration.tcpPort,
-          'ignoreChecksum': obj.configuration.ignoreChecksum,
-          'mainBatt': obj.configuration.mainBatt,
-          'auxBatt': obj.configuration.auxBatt,
-          'solar': obj.configuration.solar
-        }]}
+      obj.configuration.vedirect = [{
+        'device':'TCP',
+        'connection': obj.configuration.host,
+        'port': obj.configuration.tcpPort,
+        'ignoreChecksum': obj.configuration.ignoreChecksum,
+        'mainBatt': obj.configuration.mainBatt,
+        'auxBatt': obj.configuration.auxBatt,
+        'solar': obj.configuration.solar
+      }]
     }
 
     let dataOut = JSON.stringify(obj, null, 2)
@@ -55,6 +52,3 @@ fs.readFile(path.resolve(jsonPath, 'vedirect-signalk.json'), 'UTF-8', (err, data
     });
   }
 });
-
-
-
